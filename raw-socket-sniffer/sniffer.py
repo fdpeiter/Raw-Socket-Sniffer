76be994d52f98d8c14adc96f3ad904791d359311
Felipe Peiter <felipe.peiter@technobox.com.br>
2017-04-28 17:04:25 -0300

    Definidas estruturas de dados para o armazenamento

--- raw-socket-sniffer/sniffer.py
+++ raw-socket-sniffer/sniffer.py
@@ -8,11 +8,9 @@ ARP_PROTO_CODE = 0x806
 IPV4_PROTO_CODE = 0x800
 IPV6_PROTO_CODE = 0x86DD
 
-# Contadores
-total = arp = ipv4 = ipv6 = icmp = icmpv6 = udp = tcp = 0
-
-
 def main():
+    # Contadores
+    total_count = arp_count = ipv4_count = ipv6_count = icmp_count = icmpv6_count = udp_count = tcp_count = 0
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
         raw_data, addr = conn.recvfrom(BUFFER_SIZE)
@@ -21,6 +19,7 @@ def main():
         print eth
 
         if eth.proto == ARP_PROTO_CODE:
+            arp_count += 1
             arp = ARP(raw_data)
             print arp
 
